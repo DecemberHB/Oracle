3df0a4202fa47fbc5cc8300021ae39ecb410da95
27hyobin <27hyobin@gmail.com>
2025-07-16 17:42:37 +0900

    SQL 250716-1742

--- scott.sql
+++ scott.sql
@@ -43,5 +43,65 @@ SELECT ename ||'--'|| job FROM emp;
 
 SELECT ename||' ''s job is '|| job "NAME AND JOB" FROM emp;
 
-SELECT name,id,weight FROM STUDENT;
+SELECT name||'''s '||id||weight FROM STUDENT;
+
+SELECT empno, ename FROM emp WHERE EMPNO=7900;
+
+SELECT empno,ename,sal FROM emp WHERE ename='SMITH';
+SELECT ename, hiredate FROM emp;
+
+SELECT empno, ename, sal 
+    FROM emp
+    WHERE sal >=2000
+    and sal<=3000;
+    
+SELECT ename FROM emp -- emp 테이블에서 ename을 정렬한 후 jams martin 사이의 인물 출력
+WHERE ename BETWEEN 'JAMES' AND 'MARTIN'
+ORDER BY ename;
+
+SELECT ename FROM emp ORDER BY ename; 
+
+SELECT empno, ename, deptno FROM emp WHERE deptno IN (10,20);
+
+SELECT empno, ename, comm FROM emp WHERE deptno IN (20,30);
+
+SELECT empno, ename, comm FROM emp WHERE comm IS NULL;
+
+
+SELECT ename, sal, hiredate FROM emp ORDER BY ename;
+
+SELECT deptno, sal, ename
+FROM emp
+ORDER BY deptno ASC, sal DESC;
+
+SELECT ename, sal , hiredate
+FROM emp
+WHERE sal > 1000
+ORDER BY 2,1;
+
+SELECT studno, name, deptno1, 1
+FROM student
+WHERE deptno1 =101
+INTERSECT
+SELECT profno, name, deptno, 2
+FROM professor
+WHERE deptno = 201;
+
+SELECT ename, INITCAP(ename) "INITCAP"
+FROM emp
+WHERE deptno = 10;
+
+SELECT ename, LENGTH(ename) "LENGTH", LENGTHB(ename) "LENGTH"
+FROM emp
+WHERE deptno = 10;
+
+
+SELECT 
+  name, 
+  SUBSTR(jumin, 3, 4) AS "Birthday", 
+  TO_NUMBER(SUBSTR(jumin, 3, 4)) - 1 AS "Birthday -1"
+FROM student
+WHERE deptno1 = 101;
+
+
 
